613727de00c1fdb19df12620ae9eec375f59367d
JiHyun <sjh8911@naver.com>
2017-06-20 17:02:54 +0900

    [Game] Snake dialog 추가

--- Game/GameDlg.h
+++ Game/GameDlg.h
@@ -3,7 +3,7 @@
 //
 
 #pragma once
-
+#include "SnakeGameDlg.h"
 
 // CGameDlg 대화 상자
 class CGameDlg : public CDialogEx
@@ -11,24 +11,21 @@ class CGameDlg : public CDialogEx
 // 생성입니다.
 public:
 	CGameDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+	virtual ~CGameDlg();
 
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_GAME_DIALOG };
 #endif
-
 	protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
 
-
 // 구현입니다.
 protected:
-	HICON m_hIcon;
-
-	// 생성된 메시지 맵 함수
-	virtual BOOL OnInitDialog();
-	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
-	afx_msg void OnPaint();
-	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+
+private:
+	CSnakeGameDlg* m_pSnakeGameDlg;
 };
---
+++ Game/SnakeGameDlg.cpp
@@ -0,0 +1,33 @@
+// SnakeGameDlg.cpp : 구현 파일입니다.
+//
+
+#include "stdafx.h"
+#include "Game.h"
+#include "SnakeGameDlg.h"
+#include "afxdialogex.h"
+
+
+// CSnakeGameDlg 대화 상자입니다.
+
+IMPLEMENT_DYNAMIC(CSnakeGameDlg, CDialogEx)
+
+CSnakeGameDlg::CSnakeGameDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_GAME_SNAKE, pParent)
+{
+}
+
+CSnakeGameDlg::~CSnakeGameDlg()
+{
+}
+
+void CSnakeGameDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CSnakeGameDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CSnakeGameDlg 메시지 처리기입니다.
