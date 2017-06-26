897712112e0c558cff32dc0ca81d540ba33ac6bc
JiHyun <sjh8911@naver.com>
2017-06-26 14:44:46 +0900

    #11 [Snake] GUI 연결 및 타이머 생성 SnakeGame 틀 생성

--- Game/Snake/SnakeGame.h
+++ Game/Snake/SnakeGame.h
@@ -1,8 +1,20 @@
 #pragma once
+#include "Map.h"
+#include "Snake.h"
+
 class SnakeGame
 {
+private:
+	Map *pMap;
+	Snake *pSnake;
+	int nScore;
+
 public:
-	SnakeGame();
+	explicit SnakeGame();
 	~SnakeGame();
+
+	bool SnakeCanGo();
+	void MoveStraight();
+	void ChangDirection(Direction a_dir);
 };
 
--- Game/Snake/SnakeGameDlg.h
+++ Game/Snake/SnakeGameDlg.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "SnakeGame.h"
 
 // CSnakeGameDlg 대화 상자입니다.
 
@@ -13,7 +13,8 @@ public:
 
 // 대화 상자 데이터입니다.
 	enum { IDD = IDD_GAME_SNAKE };
-
+private:
+	SnakeGame *pSnakeGame;
 protected:
 	CListBox m_listmsg;
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
@@ -21,5 +22,8 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedOk();
-	
+	afx_msg void OnTimer(UINT nIDEvent);
+	afx_msg void OnBnClickedBtnStart();
+	afx_msg void OnBnClickedExit();
+	afx_msg void OnBnClickedBtnRestart();
 };
